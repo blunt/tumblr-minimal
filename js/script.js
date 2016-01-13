ec17a8a18fa3423bfa9a6b01e6d2c339d14d1834
Heather Vandervecht <heather@res.im>
2016-01-13 11:35:12 -0500

    Proper tabbing of js file

--- js/script.js
+++ js/script.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
-	// ---------------------------------------
-	// GLOBAL VARIABLES
-	// ---------------------------------------
-	var body = $('body'),
-		postLink = $('._postlink'),
-		overlay = $('._overlay'),
-		overlayPost = $('._overlaypost'),
-		overlayPostType;
+    // ---------------------------------------
+    // GLOBAL VARIABLES
+    // ---------------------------------------
+    var body = $('body'),
+        postLink = $('._postlink'),
+        overlay = $('._overlay'),
+        overlayPost = $('._overlaypost'),
+        overlayPostType;
 
-	// ---------------------------------------
-	// POST SINGLE -- SHOW
-	// ---------------------------------------
-	function postSingleVisible(post) {
-		var postId = post.parent().attr('data-id'),
-			postContent = post.html(),
-			postDetails = $('<div>').append(post.next('.post__more').html());
-			postDetails.find('.action-item').remove();
-		postType = post.parent().attr('class').split(' ').pop();
-		overlayPost.removeClass(overlayPostType).addClass(postType).html(postContent + postDetails.html());
-		overlay.fadeIn('fast');
-		overlayPost.addClass('overlay__post--visible');
-		body.addClass('no-scroll');
-		overlayPostType = postType;
-	}
+    // ---------------------------------------
+    // POST SINGLE -- SHOW
+    // ---------------------------------------
+    function postSingleVisible(post) {
+        var postId = post.parent().attr('data-id'),
+            postContent = post.html(),
+            postDetails = $('<div>').append(post.next('.post__more').html());
 
-	postLink.click(function() {
-		postSingleVisible($(this));
-	});
+        postDetails.find('.action-item').remove();
+        postType = post.parent().attr('class').split(' ').pop();
+        overlayPost.removeClass(overlayPostType).addClass(postType).html(postContent + postDetails.html());
+        overlay.fadeIn('fast');
+        overlayPost.addClass('overlay__post--visible');
+        body.addClass('no-scroll');
+        overlayPostType = postType;
+    }
 
-	postLink.keyup(function (e) {
-	    if (e.keyCode == 13) { // Enter key
-	        postSingleVisible($(this));
-	    }
-	});
+    postLink.click(function() {
+        postSingleVisible($(this));
+    });
 
-	// ---------------------------------------
-	// POST SINGLE -- HIDE
-	// ---------------------------------------
-	function postSingleHidden(post) {
-		post.fadeOut();
-		overlayPost.removeClass('overlay__post--visible');
-		body.removeClass('no-scroll');
+    postLink.keyup(function (e) {
+        if (e.keyCode == 13) { // Enter key
+            postSingleVisible($(this));
+        }
+    });
 
-	}
+    // ---------------------------------------
+    // POST SINGLE -- HIDE
+    // ---------------------------------------
+    function postSingleHidden(post) {
+        post.fadeOut();
+        overlayPost.removeClass('overlay__post--visible');
+        body.removeClass('no-scroll');
+    }
 
-	overlay.click(function() {
-		postSingleHidden($(this));
-	}).children().click(function() {
-		return false;
-	});
+    overlay.click(function() {
+        postSingleHidden($(this));
+    }).children().click(function() {
+        return false;
+    });
 
-	$(document).keyup(function (e) {
-	    if (e.keyCode == 27) { // ESC key
-	        postSingleHidden(overlay);
-	    }
-	});
+    $(document).keyup(function (e) {
+        if (e.keyCode == 27) { // ESC key
+            postSingleHidden(overlay);
+        }
+    });
 });
